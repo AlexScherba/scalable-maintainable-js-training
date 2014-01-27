cd4e8f2971c152f6a7b2bb9c7d9017f3975f1177
Alex Scherba <scherba.alex@gmail.com>
2014-01-27 17:30:01 +0300

    Module Pattern + Event Bus

---
+++ src/calc.js
@@ -0,0 +1,49 @@
+var APP = APP || {};
+ 
+// module-"class" (constructor)
+APP.Calc = (function (APP, $, undefined) {
+	var _defaults = {
+		dataPath: 'results.json',
+		displayType: 'alert',
+		results: {},
+		result: 0
+	};
+    return function () {
+        // private variables and methods
+        var options = {};
+		this.processResult = function (params) {
+			$.extend(options, _defaults, params);
+			getData();
+            return this;
+        };
+		var setParams = function (params) {
+			$.extend(options, _defaults, params);
+            return this;
+        };
+        var getData = function () {
+			$.getJSON(options.dataPath, function(data) {
+				options.results = data;
+				checkResults();
+			});
+        };
+		var checkResults = function () {
+			for (var x=0; options.results.length; x++)
+			{
+				if(options.result <= options.results[x].to)
+				{
+					displayResult(options.results[x].status);
+					return;
+				}
+			}
+		};
+		var displayResult = function (result) {
+			if(options.displayType == 'alert'){
+				alert(result);
+				$(options.element).html('Test passed!');
+			}else{
+				$(options.element).html(result);
+			}
+		};
+		return this;
+    };
+}(APP, jQuery));
